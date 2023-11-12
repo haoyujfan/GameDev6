89dbb73a984165ba10eaed7bfc85e76f9a07925e
Sam Wang <50272852+whaledarn@users.noreply.github.com>
2023-11-11 21:41:40 -0600

    added character pack and player

---
+++ src/body.h
@@ -0,0 +1,29 @@
+#ifndef BODY_H
+#define BODY_H
+
+#include <godot_cpp/classes/character_body3d.hpp>
+#include <godot_cpp/classes/input.hpp>
+#include <godot_cpp/classes/ray_cast3d.hpp>
+
+namespace godot {
+class PlayerCamera;
+
+class Body : public CharacterBody3D {
+    GDCLASS(Body, CharacterBody3D)
+
+private:
+    Input* input;
+
+protected:
+    static void _bind_methods();
+
+public:
+    Body();
+    ~Body();
+    void _ready() override;
+    void _process(double delta) override;
+};
+
+}
+
+#endif
